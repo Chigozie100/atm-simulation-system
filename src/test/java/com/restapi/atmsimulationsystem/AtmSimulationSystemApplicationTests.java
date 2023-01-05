ec139b60931b58e1d6565266cde24fac8f0faf38
Chigozie Nwigwe <chigozieprincenwigwe@gmail.com>
2023-01-05 14:36:04 +0100

    database configuration

---
+++ src/test/java/com/restapi/atmsimulationsystem/AtmSimulationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.atmsimulationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSimulationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
